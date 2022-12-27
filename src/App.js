c86e593aab3b09672d50397452608c7e6260cf6d
alex2231 <106402311+alex2231@users.noreply.github.com>
2022-12-27 11:01:30 -0500

    Cleaned up the folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-        <br />
-      </header>
-
-      <div className="source-code">
-        <a href="https://github.com/allemisca/React-Application">
-          Open-source code,
-        </a>{" "}
-        by Alexandra Misca
-      </div>
-    </div>
-  );
+export default function App() {
+  return <div className="App">Weather App</div>;
 }
-
-export default App;
