521d585a42e156949f200b83df62c28af596e3e6
alex2231 <106402311+alex2231@users.noreply.github.com>
2022-12-27 11:09:16 -0500

    Added the Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Alexandra Misca and it is{" "}
+        <a
+          href="https://github.com/allemisca/React-Application"
+          target="_blank"
+        >
+          {" "}
+          open-sourced.
+        </a>
+      </footer>
+    </div>
+  );
 }
